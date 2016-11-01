8183e651dd07253ec70f4f1462f93f536a4c9a0d
Ollie O'Donnell <oliverodaa@gmail.com>
2016-11-01 06:55:15 +0000

    added default_charityA and default_charityB to seeds and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,3 @@
-
-games = [{:title => 'Tutorial1', :description =>'This is the tutorial for the Giving Game', :charityA_title => 'Against Malaria Foundation', :charityB_title => 'Give Directly', :tutorial => true, :show_results => true, :is_private => false, :resource_id => "t1"},
-        {:title => 'Tutorial2', :description =>'This is the second tutorial for the Giving Game', :charityA_title => 'Charity A', :charityB_title => 'Charity B', :tutorial => true, :show_results => true, :is_private => false, :resource_id => "t2"},
-        {:title => 'Game 1', :description =>'This is not a tutorial', :charityA_title => 'Charity A', :charityB_title => 'Charity B', :tutorial => false, :show_results => true, :is_private => false, :resource_id => "nt1"},
-        {:title => 'Expired Game 1', :description =>'This is an expired game', :charityA_title => 'Charity 1', :charityB_title => 'Charity 2', :tutorial => false, :show_results => false, :expired => true, :is_private => false, :resource_id => "e1"},
-        {:title => 'Expired Game 2', :description =>'This is also an expired game', :charityA_title => 'Charity Uno', :charityB_title => 'Charity Dos', :tutorial => false, :show_results => false, :expired => true, :is_private => false, :resource_id => "e2"},
-        {:title => 'Expired Game 3', :description =>'This is the last expired game', :charityA_title => 'Charity One', :charityB_title => 'Charity Two', :tutorial => false, :show_results => true, :expired => true, :is_private => false, :resource_id => "e3"}]
-games.each do |game|
-    GivingGame.create!(game)
-end
-
-user = User.create! :email => 'jo@trader.com', :password => 'lulzlulz', :password_confirmation => 'lulzlulz', :username => 'Trader_Joe123', :giving_games => GivingGame.all
-
 malaria_description = 'Malaria is one of the leading killers of children in Africa.
 But with insecticide-treated bed nets we can prevent deaths and 
 many other non-fatal cases of malaria
@@ -44,3 +31,18 @@ charities = [{:name => 'Against Malaria Foundation', :description => malaria_des
 charities.each do |game|
     Charity.create!(game)
 end
+
+charA = Charity.all[0].id
+charB = Charity.all[1].id
+
+games = [{:title => 'Tutorial1', :description =>'This is the tutorial for the Giving Game', :charityA_title => 'Against Malaria Foundation', :charityB_title => 'Give Directly', :tutorial => true, :show_results => true, :is_private => false, :resource_id => "t1", :default_charity_a => charA, :default_charity_b => charB},
+        {:title => 'Tutorial2', :description =>'This is the second tutorial for the Giving Game', :charityA_title => 'Charity A', :charityB_title => 'Charity B', :tutorial => true, :show_results => true, :is_private => false, :resource_id => "t2", :default_charity_a => charA, :default_charity_b => charB},
+        {:title => 'Game 1', :description =>'This is not a tutorial', :charityA_title => 'Charity A', :charityB_title => 'Charity B', :tutorial => false, :show_results => true, :is_private => false, :resource_id => "nt1", :default_charity_a => charA, :default_charity_b => charB},
+        {:title => 'Expired Game 1', :description =>'This is an expired game', :charityA_title => 'Charity 1', :charityB_title => 'Charity 2', :tutorial => false, :show_results => false, :expired => true, :is_private => false, :resource_id => "e1", :default_charity_a => charA, :default_charity_b => charB},
+        {:title => 'Expired Game 2', :description =>'This is also an expired game', :charityA_title => 'Charity Uno', :charityB_title => 'Charity Dos', :tutorial => false, :show_results => false, :expired => true, :is_private => false, :resource_id => "e2", :default_charity_a => charA, :default_charity_b => charB},
+        {:title => 'Expired Game 3', :description =>'This is the last expired game', :charityA_title => 'Charity One', :charityB_title => 'Charity Two', :tutorial => false, :show_results => true, :expired => true, :is_private => false, :resource_id => "e3", :default_charity_a => charA, :default_charity_b => charB}]
+games.each do |game|
+    GivingGame.create!(game)
+end
+
+user = User.create! :email => 'jo@trader.com', :password => 'lulzlulz', :password_confirmation => 'lulzlulz', :username => 'Trader_Joe123', :giving_games => GivingGame.all
